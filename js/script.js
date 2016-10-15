ef1d3d1c76a76fe94345618ce4456059cbe63d1f
leaTrinel <leatrinel1@gmail.com>
2016-10-15 14:20:45 +0200

    animation menu sandwich + design formulaire

--- js/script.js
+++ js/script.js
@@ -1,13 +1,24 @@
 $( document ).ready(function() {
 
-    var largeur_fenetre = $(window).width();
-    var hauteur_fenetre = $(window).height();
-//    $("...").css({
-//        'width': largeur_fenetre
-//    });
-    
-     $(window).resize(function(){
-    
+    //    var largeur_fenetre = $(window).width();
+    //    var hauteur_fenetre = $(window).height();
+
+    //open nav
+    $("#bouton-nav-open").click(function(){
+        $("nav ul").animate({
+            marginLeft: 0
+        }, 400);
     });
-    
+
+    //close nav
+    $("#bouton-nav-close").click(function(){
+        $("nav ul").animate({
+            marginLeft: "-100%"
+        }, 400);
+    });
+
+    $(window).resize(function(){
+
+    });
+
 });
