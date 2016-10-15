00b4aad82683d9c5f464da3e9e95d87ce9cc229e
leaTrinel <leatrinel1@gmail.com>
2016-10-15 12:32:21 +0200

    Contenu page accueil + Navigation

---
+++ js/script.js
@@ -0,0 +1,13 @@
+$( document ).ready(function() {
+
+    var largeur_fenetre = $(window).width();
+    var hauteur_fenetre = $(window).height();
+//    $("...").css({
+//        'width': largeur_fenetre
+//    });
+    
+     $(window).resize(function(){
+    
+    });
+    
+});
